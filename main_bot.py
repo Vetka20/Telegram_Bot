a3c5cdfe71381c90f502571a542ed61af0d379fc
Vetka20 <961497869@ukr.net>
2023-02-03 19:04:37 +0200

    create package and add threading(Fail)

--- main_bot.py
+++ main_bot.py
@@ -6,14 +6,16 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.types.message import ContentType
 
 import bot_image
-import asyncio
 import logging
+import asyncio
+
 
 bot = Bot(token=TOKEN)
 
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
 logging.basicConfig(level=logging.INFO)
+
 
 @dp.message_handler(commands="start")
 async def hello(message: types.Message):
@@ -28,23 +30,50 @@ class State_for_image(StatesGroup):
     
     waiting_image = State()
     waiting_parametrs = State()
+    waiting_set_parametr_1 = State()
+    waiting_set_parametr_1 = State()
+
+@dp.message_handler(commands="cancel", state = "*")
+@dp.message_handler(regexp="отмена",state="*")
+@dp.message_handler(regexp="Cancel",state="*")
+async def cancel(message: types.Message, state: FSMContext):
+    await message.answer("Cancel")
+    await state.finish()
+    
 
 @dp.message_handler(commands="create_image", state="*")
 async def image_start(message: types.Message, state: FSMContext):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     keyboard.add("Upload image")
+    keyboard.add("Custom color")
     
     keyboard.add("Cancel")
     await message.answer("Chose what step you want?", reply_markup=keyboard)
     await state.set_state(State_for_image.waiting_parametrs)
 
+@dp.message_handler(state=State_for_image.waiting_set_parametr_1)
+async def set_parametr_1(message: types.Message, state: FSMContext):
+    
+    if message.text.lower == "all":
+        message.answer("Done")
+        state.set_state(State_for_image.waiting_parametrs)
+    
+
 @dp.message_handler(state=State_for_image.waiting_parametrs)
 async def set_parametrs_image(message: types.Message, state: FSMContext):
-    if message.text.lower()!="upload image":
-        await message.answer("You didn`t choise the button, please push the button or send 'cancel'")
-        return
-    await message.answer("Wait your Photo")
-    await state.set_state(State_for_image.waiting_image)
+    match message.text.lower():
+        case "upload image":
+            await message.answer("Wait your Photo")
+            await state.set_state(State_for_image.waiting_image)
+        case "custom color":
+            await message.answer("Change color palitr")
+            await state.set_state(State_for_image.waiting_set_parametr_1)
+        
+        case _:
+            await message.answer("You didn`t choise the button, please push the button or send 'cancel'")
+            return
+        
+    
 
 @dp.message_handler(content_types=ContentType.PHOTO,state=State_for_image.waiting_image)
 async def upload_image(message: types.Message, state: FSMContext):
@@ -58,8 +87,9 @@ async def upload_image(message: types.Message, state: FSMContext):
     
 @dp.message_handler(commands="remake", state='*')
 async def remake_result_image(message: types.Message, state: FSMContext):
-    bot_image.correct(id=message.from_id ,src_image=f"/home/vetka/Telegram_Bot/images/{message.from_id}/image.jpg")
     await message.answer("I start create result image and countour, please wait")
+    bot_image.correct(id=message.from_id ,src_image=f"/home/vetka/Telegram_Bot/images/{message.from_id}/image.jpg")
+    await message.answer("I finish remake image. Now use comamnd /get_image")
 
 @dp.message_handler(commands="get_image", state="*")
 async def send_result_image(message: types.Message, state: FSMContext):
@@ -67,13 +97,7 @@ async def send_result_image(message: types.Message, state: FSMContext):
     await bot.send_photo(chat_id=message.from_id, photo=photo)
     
 
-@dp.message_handler(commands="cancel", state = "*")
-@dp.message_handler(regexp="отмена",state="*")
-@dp.message_handler(regexp="Cancel",state="*")
-async def cancel(message: types.Message, state: FSMContext):
-    await message.answer("Cancel")
-    await state.finish()
-    
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
